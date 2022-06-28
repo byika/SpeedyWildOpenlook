d31cd9c3a28207c493d3539d2b607aca83801e48
Gala Kostina <anitsokaknil@gmail.com>
2022-06-28 16:03:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication39/ConsoleApplication39.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int height = 0; // высота треугольника
+	int step = 0;
+
+	cout << "BblCOTA: ";
+	cin >> height;
+	cout << "KOLBO YAPYCOB: ";
+	cin >> step;
+	for (int k = 0; k < step; k++) {
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 1; j < height - i; j++)
+			{
+				cout << "  ";
+			}
+			for (int j = k ; j <= height; j++)
+			{
+				cout << "  ";
+			}
+			for (int j = (height - 2 * i) - (2 * k); j <= height; j++)
+			{
+				cout << "<>";
+			}
+			cout << "\n";
+		}
+	}
+
+	for (int i = 0; i < height / 2; i++) {
+		for (i = 0; i < height + step ; i++) {
+			cout << "  ";
+		}
+		for (int j = 1; j  < step ; j++) {
+			cout << "<>";
+		}
+		cout << "\n";
+	}
+
+}
